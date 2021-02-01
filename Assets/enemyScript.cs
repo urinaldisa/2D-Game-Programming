59d67b00a36900d335fd1e1ca578b8c635ed881b
Khaibar Abrar <V0ster@Moozarttt.local>
2021-02-01 13:05:47 +0700

    first commit

---
+++ Assets/enemyScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyScript : MonoBehaviour {
+	Vector2 randomPoint;
+	float randomX, randomY;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		EnemyMoveRandom();
+	}
+	void EnemyMoveRandom(){
+		randomPoint = new Vector2(randomX, randomY);
+		transform.position = Vector2.MoveTowards(transform.position, randomPoint, 5* Time.deltaTime);
+		if(Vector2.Distance(transform.position, randomPoint) <= 0)
+		{
+			randomX = Random.Range(-8.81f, 8.3f);
+			randomY = Random.Range(-4.66f, 4.67f);
+		}
+	}
+// 		private void OnTriggerEnter2D(Collider2D collision)
+// 	{
+// 		if(collision.gameObject.tag=="bullet")
+// 		{
+// 			Destroy(gameObject);
+// 		}
+// }
+}
---
+++ Assets/playerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+	Rigidbody2D Rb;
+	public float ms;
+	public float jf;
+	// Use this for initialization
+	void Start () {
+		Rb = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horiz = Input.GetAxisRaw("Horizontal"); //A,D movement
+		Rb.velocity = new Vector2(ms *horiz,Rb.velocity.y);
+		if(Input.GetButtonDown("Jump"))
+		{
+			Rb.AddForce(new Vector2(0,jf));
+		}
+	}
+	/// Sent when another object enters a trigger collider attached to this
+	/// object (2D physics only).
+	/// </summary>
+	/// <param name="other">The other Collider2D involved in this collision.</param>
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.CompareTag("Finish"))
+			{
+				Destroy(gameObject);
+			}
+			if(other.CompareTag("Respawn"))
+			{
+				Destroy(gameObject);
+			}
+			if(other.CompareTag("Coins"))
+			{
+				Destroy(other.gameObject);
+			}
+	}
+}
